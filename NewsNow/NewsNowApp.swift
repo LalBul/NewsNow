a54dad12780991570dddeb6500d15650054c1a07
VladimirS <serbinvova1337@gmail.com>
2025-05-31 21:36:47 +0300

    Initial Commit

---
+++ NewsNow/NewsNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsNowApp.swift
+//  NewsNow
+//
+//  Created by Владимир Сербин on 31.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsNowTests/NewsNowTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsNowTests.swift
+//  NewsNowTests
+//
+//  Created by Владимир Сербин on 31.05.2025.
+//
+
+import Testing
+@testable import NewsNow
+
+struct NewsNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
